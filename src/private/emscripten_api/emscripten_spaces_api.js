9a97a68e8a3ffa6fbc19db96afc5e73f9f582de4
Paul Harris <paul.harris@eegeo.com>
2016-07-07 16:45:14 +0100

    Add internal API point for picking terrain points
    Buddy: Ian

--- src/private/emscripten_api/emscripten_spaces_api.js
+++ src/private/emscripten_api/emscripten_spaces_api.js
@@ -6,7 +6,11 @@ function EmscriptenSpacesApi(apiPointer, cwrap) {
     var _apiPointer = apiPointer;
 
     var _worldToScreenWrap = cwrap(
-        "worldToScreen", "number", ["number", "number", "number", "number", "number"]);
+        "worldToScreen", null, ["number", "number", "number", "number", "number"]);
+
+    var _screenToTerrainPointWrap = cwrap(
+        "screenToTerrainPoint", "number", ["number", "number", "number", "number"]);
+
 
     var _worldToScreen = function(lat, long, alt) {
         var screenPos = [0, 0, 0];
@@ -17,10 +21,26 @@ function EmscriptenSpacesApi(apiPointer, cwrap) {
         return new space.Vector3(screenPos);
     };
 
+    var _screenToTerrainPoint = function(screenX, screenY) {
+        var latLngAltArray = [0, 0, 0];
+        var foundWorldPoint = false;
+        emscriptenMemory.passDoubles(latLngAltArray, function(resultArray, arraySize) {
+            var success = _screenToTerrainPointWrap(_apiPointer, screenX, screenY, resultArray);
+            foundWorldPoint = !!success;
+            latLngAltArray = emscriptenMemory.readDoubles(resultArray, 3);
+        })
+        return (foundWorldPoint) ? L.latLng(latLngAltArray) : null;
+    };
+
 
     this.worldToScreen = function(position) {
         var point = L.latLng(position);
         return _worldToScreen(point.lat, point.lng, point.alt || 0);
+    };
+
+    this.screenToTerrainPoint = function(screenPoint) {
+        var point = L.point(screenPoint);
+        return _screenToTerrainPoint(point.x, point.y);
     };
 }
 
