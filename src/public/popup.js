14704b948c80afbadf2767d2945c2d5180246d0f
jlunderwood <judith.underwood@eegeo.com>
2017-01-03 12:37:42 +0000

    Override leaflet's content update for popups to avoid repeated re-creation of HTML content. The base leaflet behaviour is incompatible with HTML content in POI views. Buddy: John

--- src/public/popup.js
+++ src/public/popup.js
@@ -12,6 +12,30 @@ var Popup = L.Popup.extend({
         return this;
     },
 
+	_updateContent: function () {
+		if (!this._content) { return; }
+
+		var node = this._contentNode;
+		var content = (typeof this._content === 'function') ? this._content(this._source || this) : this._content;
+
+        var contentNeedsUpdate = true;
+        if (content.outerHTML && content.outerHTML === node.innerHTML) {
+            // This test will fail to detect changes which don't affect HTML, but otherwise the DOM for the popup is
+            // rebuilt every update cycle.  This makes embedding external HTML impossible.
+            contentNeedsUpdate = false;
+        }
+
+		if (typeof content === 'string') {
+			node.innerHTML = content;
+		} else if (!node.hasChildNodes() || contentNeedsUpdate) {
+			while (node.hasChildNodes()) {
+				node.removeChild(node.firstChild);
+			}
+			node.appendChild(content);
+		}
+		this.fire('contentupdate');
+	},
+
     _updatePosition: function() {
         if (!this._map) { return; }
 
