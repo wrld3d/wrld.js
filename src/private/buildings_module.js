7501704aedb2c8131d238b8aeb5f41f49950373a
BPaskeeeGeo <ben.paske@eegeo.com>
2017-08-29 15:42:01 +0100

    Adding building footprint fetching.
    Buddy: Micky

--- src/private/buildings_module.js
+++ src/private/buildings_module.js
@@ -3,16 +3,19 @@ var MapModule = require("./map_module");
 function BuildingsModule(emscriptenApi) {
     var _emscriptenApi = emscriptenApi;
     var _ready = false;
-    
+
     this.getBuildingFootprintAtLocation = function(latitude, longitude, altitude, callback) {
         if(_ready) {
             _emscriptenApi.buildingsApi.getBuildingFootprintAtLocation(latitude, longitude, altitude, callback);
         }
+        else {
+            callback(false);
+        }
     };
 
     this.onInitialized = function() {
-        _ready = true;
         _emscriptenApi.buildingsApi.setEventCallback();
+        _ready = true;
     };
 }
 
--- src/private/emscripten_api/emscripten_buildings_api.js
+++ src/private/emscripten_api/emscripten_buildings_api.js
@@ -4,18 +4,19 @@ function EmscriptenBuildingsApi(apiPointer, cwrap, runtime, emscriptenMemory) {
     var _callbackMap = {};
     var _apiPointer = apiPointer;
     var _emscriptenMemory = emscriptenMemory;
-    
+
     function apiCallback(requestId, buildingReceived, baseAlt, topAlt, centroidLat, centroidLong, outline, outlineSize) {
-        console.log(outline);
-        console.log(outlineSize);
         var outlineArray = _emscriptenMemory.readDoubles(outline, outlineSize);
-        console.log(outlineArray[0]);
-        console.log(outlineArray[1]);
-        console.log(outlineArray[2]);
-        _callbackMap[requestId](buildingReceived, baseAlt, topAlt, centroidLat, centroidLong);
+        var outline2dArray = [];
+        while (outlineArray.length)
+        {
+            outline2dArray.push(outlineArray.splice(0,3));
+        }
+
+        _callbackMap[requestId](buildingReceived, baseAlt, topAlt, centroidLat, centroidLong, outline2dArray);
         delete _callbackMap[requestId];
     }
-    
+
     this.setEventCallback = function() {
         cwrap("SetBuildingsApiEventCallback", null, ["number", "number"])(_apiPointer, runtime.addFunction(apiCallback));
     };
@@ -26,7 +27,6 @@ function EmscriptenBuildingsApi(apiPointer, cwrap, runtime, emscriptenMemory) {
         _callbackMap[_requestCounter] = callback;
         _getBuildingFootprintAtLocationInterop(_apiPointer, latitude, longitude, altitude, _requestCounter);
         _requestCounter++;
-        
     };
 }
 
