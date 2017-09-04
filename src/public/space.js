ab5ef2c680b1c31fd17509b8662082556d397d80
MichaelChanEegeo <michael.chan@kimbleoperations.com>
2017-09-04 10:52:13 +0100

    Modifying zoom to distance and distance to zoom methods to interpolate deciamal values. Buddy: Calum L

--- src/public/space.js
+++ src/public/space.js
@@ -61,6 +61,10 @@ var _altitudes = [
     5
 ];
 
+var _lerp = function (a,  b,  c) {
+    return a + c * (b - a);
+}
+
 var _altitudeToZoom = function(altitude, comparisonFunc) {
     var zoom = _altitudes.findIndex(function(zoomLevelDistance) {
         return comparisonFunc(altitude, zoomLevelDistance);
@@ -69,38 +73,53 @@ var _altitudeToZoom = function(altitude, comparisonFunc) {
     return (zoom === -1) ? maxZoom : zoom;
 };
 
-
-var zoomToDistance = function(zoom) {
-    if(zoom < 0) {
-        return _altitudes[0];
-    }
-    else if (zoom >= _altitudes.length) {
-        return _altitudes[_altitudes.length - 1];
-    }
-    return _altitudes[zoom];
-};
-
-var distanceToZoom = function(distance) {
-    return nearestZoomAbove(distance);
-};
-
-var nearestZoomAbove = function(distance) {
+var _nearestZoomAbove = function(distance) {
     var zoomAbove = _altitudeToZoom(distance, function(d, z) { return d > z; });
     return Math.max(0, zoomAbove - 1);
 };
 
-var nearestZoomBelow = function(distance) {
+var _nearestZoomBelow = function(distance) {
     return _altitudeToZoom(distance, function(d, z) { return d >= z; });
 };
 
+var zoomToDistance = function(zoom) {
+    var zoomlevel = zoom;
+    if(zoomlevel < 0) {
+        zoomlevel =  0;
+    }
+    else if (zoomlevel >= _altitudes.length) {
+        zoomlevel = _altitudes.length - 1;
+    }
+
+    var nearestZoomBelow = Math.floor(zoomlevel);
+    var nearestZoomAbove = Math.ceil(zoomlevel);
+    var valueBetweenNearestZoomLevels = zoomlevel - nearestZoomBelow;
+    return _lerp(_altitudes[nearestZoomBelow], _altitudes[nearestZoomAbove], valueBetweenNearestZoomLevels);
+};
+
+var distanceToZoom = function(distance) {
+    var smallestAltitude = _altitudes.length - 1;
+    if(distance < _altitudes[smallestAltitude])
+    {
+        distance = _altitudes[smallestAltitude];
+    }
+    if(distance > _altitudes[0])
+    {
+        distance = _altitudes[0];
+    }
+    var nearestZoomAbove = _nearestZoomAbove(distance);
+    var nearestZoomBelow = _nearestZoomBelow(distance);
+    var distanceBetweenNearestZoomLevels = _altitudes[nearestZoomAbove] - _altitudes[nearestZoomBelow];
+    var distanceFromZoomLevelBelow = distance - _altitudes[nearestZoomBelow];
+
+    return (nearestZoomAbove === nearestZoomBelow) ? nearestZoomBelow : _lerp(nearestZoomBelow, nearestZoomAbove, distanceFromZoomLevelBelow / distanceBetweenNearestZoomLevels);
+};
 
 var space = {
     Vector3: Vector3,
     Vector4: Vector4,
     zoomToDistance: zoomToDistance,
-    distanceToZoom: distanceToZoom,
-    nearestZoomAbove: nearestZoomAbove,
-    nearestZoomBelow: nearestZoomBelow
+    distanceToZoom: distanceToZoom
 };
 
 module.exports = space;
