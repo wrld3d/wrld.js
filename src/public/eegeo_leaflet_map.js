8125db3404347db2325f6c6cd522f782af5e720d
jean.grizet <jean.grizet@wrld3d.com>
2018-01-16 17:25:38 +0000

    Fix for MPLY 9839: Introduce new displayOption flag beheavior for leaflet layers added to wrld map Buddy: sam.ainsworth

--- src/public/eegeo_leaflet_map.js
+++ src/public/eegeo_leaflet_map.js
@@ -463,6 +463,9 @@ var EegeoLeafletMap = L.Map.extend({
         var currentMapStateIsOutdoors = !this.indoors.isIndoors();
         var layerIsOutdoors = !indoorOptions.hasIndoorMap(layer);
 
+		if (layer.options.displayOption === 'currentMap')
+			return true;
+
         if (currentMapStateIsOutdoors)
         {
             return layerIsOutdoors;
