b449adfd23293f839972c0b35a40e5a6c00a3585
Ali Arslan <ali.arslan@wrld3d.com>
2021-01-01 19:31:56 +0500

    Exposed indoorSelectionTimeoutDuration and indoorTransitionFadeDuration through WRLD Map options.

--- src/private/eegeo_map_controller.js
+++ src/private/eegeo_map_controller.js
@@ -67,7 +67,9 @@ var EegeoMapController = function (mapId, emscriptenApi, domElement, apiKey, bro
         idleSecondsBeforeFrameRateThrottle: 30.0,
 
         drawClearColor: "#000000ff",
-        indoorMapBackgroundColor: "#000000c0"
+        indoorMapBackgroundColor: "#000000c0",
+        indoorSelectionTimeoutDuration: 30.0,
+        indoorTransitionFadeDuration: 0.5
     };
 
     options = L.extend(_defaultOptions, options);
@@ -131,6 +133,8 @@ var EegeoMapController = function (mapId, emscriptenApi, domElement, apiKey, bro
     var trafficEnabled = (options.trafficEnabled) ? "1" : "0";
     var trafficDisableWhenEnteringIndoorMaps = (options.trafficDisableWhenEnteringIndoorMaps) ? "1" : "0";
     var indoorLabelsAlwaysHidden = (options.indoorLabelsAlwaysHidden) ? "1" : "0";
+    var indoorSelectionTimeoutDuration = options.indoorSelectionTimeoutDuration;
+    var indoorTransitionFadeDuration = options.indoorTransitionFadeDuration;
 
     _Module["arguments"] = [
         _canvasId,
@@ -149,7 +153,9 @@ var EegeoMapController = function (mapId, emscriptenApi, domElement, apiKey, bro
         _containerId,
         trafficEnabled,
         trafficDisableWhenEnteringIndoorMaps,
-        indoorLabelsAlwaysHidden
+        indoorLabelsAlwaysHidden,
+        indoorSelectionTimeoutDuration.toString(),
+        indoorTransitionFadeDuration.toString()
     ];
 
     this.leafletMap = new EegeoLeafletMap(
