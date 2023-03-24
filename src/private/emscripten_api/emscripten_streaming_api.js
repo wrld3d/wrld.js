b3dc1430527f41ad2ccadd694103396d763ea1bb
Sam A <sam.ainsworth@wrld3d.com>
2023-03-24 10:01:56 +0000

    Fixes for steamin API

--- src/private/emscripten_api/emscripten_streaming_api.js
+++ src/private/emscripten_api/emscripten_streaming_api.js
@@ -2,8 +2,8 @@ export function EmscriptenStreamingApi(emscriptenApiPointer, cwrap, emscriptenMo
 
     var _emscriptenApiPointer = emscriptenApiPointer;
     
-    var _streamingApi_SetStreamingCompletedCallback = cwrap("streamingApi_SetStreamingCompletedCallback", "number", ["number", "number"]);
-    var _streamingApi_SetStreamingStartedCallback = cwrap("streamingApi_SetStreamingStartedCallback", "number", ["number", "number"]);
+    var _streamingApi_SetStreamingCompletedCallback = cwrap("streamingApi_SetStreamingCompletedCallback", null, ["number", "number"]);
+    var _streamingApi_SetStreamingStartedCallback = cwrap("streamingApi_SetStreamingStartedCallback", null, ["number", "number"]);
 
     this.registerStreamingCompletedCallback = (callback) => {
         _streamingApi_SetStreamingCompletedCallback(_emscriptenApiPointer, emscriptenModule.addFunction(callback, "v"));
